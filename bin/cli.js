41ea0e0641f627084736937eaedac870f95f2f9c
9996rojit <rockman9996@gmail.com>
2023-06-01 21:51:52 +0545

    Managed version and clone script

--- bin/cli.js
+++ bin/cli.js
@@ -11,7 +11,7 @@ const runCommand = command => {
   return true;
 }
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone--depth 1 https://github.com/logxero/cli-ts-backend.git ${repoName}`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/logxero/cli-ts-backend.git ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`
 
 console.log(`Cloning the repository with name ${repoName}`);
